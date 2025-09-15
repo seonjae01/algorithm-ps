0f81b6fa4efd3287746f26e2c1c7553fbbd18a29
seonjae01 <tjswo5536@naver.com>
2025-09-15 17:52:40 +0900

    [Silver III] Title: N과 M (5), Time: 24 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+vector<int> nums;
+vector<int> path;
+vector<bool> visited(10001);
+
+void solve()
+{
+    if (path.size() == m)
+    {
+        for (auto p : path)
+            cout << p << ' ';
+        cout << '\n';
+        return;
+    }
+
+    for (auto num : nums)
+    {
+        if (visited[num])
+            continue;
+
+        visited[num] = true;
+        path.push_back(num);
+        solve();
+        visited[num] = false;
+        path.pop_back();
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> n >> m;
+    nums.resize(n);
+    for (int i = 0; i < n; i++)
+        cin >> nums[i];
+
+    sort(nums.begin(), nums.end());
+    solve();
+
+    return 0;
+}
