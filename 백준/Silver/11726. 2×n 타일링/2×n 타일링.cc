34192d9590f373a9ef8a00250083441aa77713f6
seonjae01 <tjswo5536@naver.com>
2025-09-24 14:47:37 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, dp[1001] = { 0, 1, 2 };
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> n;
+
+    for (int i = 3; i <= n; i++)
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+
+    cout << dp[n] << '\n';
+
+    return 0;
+}
