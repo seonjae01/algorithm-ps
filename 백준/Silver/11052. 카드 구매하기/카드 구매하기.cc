ff60a097fcb05694c450f20d82c220ae57fbcc76
seonjae01 <tjswo5536@naver.com>
2025-09-24 15:53:43 +0900

    [Silver I] Title: 카드 구매하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, dp[1001];
+vector<int> cards;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> n;
+    cards.resize(n + 1);
+
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> cards[i];
+        dp[i] = cards[i];
+
+        for (int j = 1; j <= i; j++)
+            dp[i] = max(dp[i], dp[j] + dp[i - j]);
+    }
+
+    cout << dp[n] << '\n';
+
+    return 0;
+}
