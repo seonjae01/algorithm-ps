05a719f138601640a4fcd546a1c03911634ae38f
seonjae01 <tjswo5536@naver.com>
2025-09-29 17:49:25 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, mx, dp[1001];
+vector<int> nums;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> n;
+    nums.resize(n + 1);
+
+    for (int i = 1; i <= n; i++)
+        cin >> nums[i];
+
+    for (int i = 1; i <= n; i++)
+    {
+        dp[i] = 1;
+        for (int j = 1; j < i; j++)
+        {
+            if (nums[j] < nums[i])
+                dp[i] = max(dp[i], dp[j] + 1);
+        }
+        mx = max(mx, dp[i]);
+    }
+
+    cout << mx << '\n';
+
+    vector<int> path(mx);
+    for (int i = n; i > 0; i--)
+    {
+        if (dp[i] == mx)
+            path[--mx] = nums[i];
+    }
+
+    for (auto p : path)
+        cout << p << ' ';
+
+    return 0;
+}
