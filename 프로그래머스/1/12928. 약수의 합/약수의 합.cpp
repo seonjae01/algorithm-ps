1b0fc0ab9b34a06e87a10340b42b49e49281cd2d
seonjae01 <tjswo5536@naver.com>
2026-03-24 09:14:37 +0900

    [level 1] Title: 약수의 합, Time: 0.01 ms, Memory: 4.01 MB -BaekjoonHub

---
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    for (int i = 1; i <= sqrt(n); i++)
+    {
+        if (n % i == 0)
+        {
+            if (i == sqrt(n))
+                answer += i;
+            else
+                answer += (n / i + i);
+        }
+    }
+
+    return answer;
+}
