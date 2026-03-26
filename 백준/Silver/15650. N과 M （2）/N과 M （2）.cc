a7c372aa5683737339903efbeb888619d4a969d0
seonjae01 <tjswo5536@naver.com>
2026-03-26 09:44:28 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+vector<int> path;
+vector<bool> visited;
+
+void solve()
+{
+    if (path.size() == m)
+    {
+        for (auto p : path)
+            cout << p << ' ';
+        cout << '\n';
+        return;
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (visited[i] || (path.size() > 0 && path.back() > i))
+            continue;
+
+        visited[i] = true;
+        path.push_back(i);
+        solve();
+        visited[i] = false;
+        path.pop_back();
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> n >> m;
+    visited.resize(n + 1, false);
+    solve();
+
+    return 0;
+}
