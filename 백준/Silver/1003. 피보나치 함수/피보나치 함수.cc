fa204de05d79bb491488a694050c89587ec5483e
seonjae01 <tjswo5536@naver.com>
2026-03-26 09:43:02 +0900

    [Silver III] Title: 피보나치 함수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long dp[41][2];
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    dp[0][0] = 1;
+    dp[1][1] = 1;
+
+    for (int i = 2; i < 41; i++)
+    {
+        dp[i][0] = dp[i - 1][0] + dp[i - 2][0];
+        dp[i][1] = dp[i - 1][1] + dp[i - 2][1];
+    }
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        cout << dp[n][0] << ' ' << dp[n][1] << '\n';
+    }
+
+    return 0;
+}
