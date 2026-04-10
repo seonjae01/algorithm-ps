b20eb261288067a8e15776d9b903c649571a7907
seonjae01 <tjswo5536@naver.com>
2026-04-10 10:07:53 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string phone_number) {
+    string answer = "";
+    for (int i = 0; i < phone_number.size(); ++i)
+        answer += i >= phone_number.size() - 4 ? phone_number[i] : '*';
+    return answer;
+}
